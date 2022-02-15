2f48c2a49604e112ae185ed5e171d86da15e0507
Brian Redmond <chzbrgr71@users.noreply.github.com>
2022-02-15 11:38:00 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 # get params from config.json file
-CONFIG="$(cat config.json | jq -r .)"
-export CONFIG
+export CONFIG="$(cat config.json | jq -r .)"
 
 export SUBSCRIPTION_ID="$(echo $CONFIG | jq -r '.subscription_id')"
 export TENANT_ID="$(echo $CONFIG | jq -r '.tenant_id')"
@@ -9,7 +8,7 @@ export PREFIX="$(echo $CONFIG | jq -r '.prefix')"
 
 # set initial variables
 export SUFFIX=$RANDOM
-export RG_NAME=$PREFIX-reddog-aks-$SUFFIX
+export RG_NAME=$PREFIX-aks-reddog-$SUFFIX
 export LOGFILE_NAME="./logs/${RG_NAME}.log"
 
 ./deploy-everything.sh $RG_NAME $LOCATION $SUFFIX 2>&1 | tee -a $LOGFILE_NAME
